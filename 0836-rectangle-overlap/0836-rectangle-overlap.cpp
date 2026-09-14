88cfb69e12e08892a00f72a0467d6749f50dea1d
Niraj Kumar Singh <nkumarsingh130@gmail.com>
2026-09-14 13:09:57 +0530

    Time: 0 ms (100%), Space: 9.7 MB (77.39%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.cpp
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -14,37 +14,6 @@ public:
         if(y1 >= y4 || y3 >= y2 || x1 >= x4 || x3 >= x2){
             return false;
         }
-
-        // if(x1 < x3){
-        //     if(x2 < x3){
-        //         return false;
-        //     }
-        //     else if(x2 == x3){
-        //         if(x2 < x4){
-        //             return false;
-        //         }
-        //     }
-        // }
-        // if(x1 == x3){
-        //     if(y1 < y3){
-        //         return false;
-        //     }
-        //     else if(y3 < y1){
-        //         if(x2 < x4){
-        //             return false;
-        //         }
-        //     }
-        // }
-        // if(x3 < x1){
-        //     if(x3 < x2){
-        //         return false;
-        //     }
-        //     else if(x3 == x2){
-        //         if(x4 < x2){
-        //             return false;
-        //         }
-        //     }
-        // }
         return true;
     }
 };
