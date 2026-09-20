59f5a37e5540332b08e0ea9984947f181f64d85e
Niraj Kumar Singh <nkumarsingh130@gmail.com>
2026-09-21 00:01:03 +0530

    Time: 0 ms (100%), Space: 15.7 MB (82.68%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -4,18 +4,15 @@ public:
         for(int i = nums.size()-2; i >= 0; i--){
             if(nums[i] < nums[i+1]){
                 int j = nums.size()-1;
-                while(j > i){
-                    if(nums[j] > nums[i]){
-                        break;
-                    }
+                while(nums[j] <= nums[i]){
                     j--;
                 }
                 swap(nums[i],nums[j]);
-                sort(nums.begin()+i+1,nums.end());
+                reverse(nums.begin()+i+1,nums.end());
                 return;
             }
         }
-        sort(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.end());
         return;
     }
 };
