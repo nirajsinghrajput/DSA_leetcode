ed351150b0e2d2146a676d639e1e0340413894bb
Niraj Kumar Singh <nkumarsingh130@gmail.com>
2026-09-23 21:05:46 +0530

    Time: 0 ms (100%), Space: 18.8 MB (78.22%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        int maxLen = 0;
-        int left = 0;
-        unordered_set<char> st;
-        for(int right = 0; right < s.length(); right++){
-            while(st.count(s[right])) {
-                st.erase(s[left]);
-                left++;
-            }
+    // int lengthOfLongestSubstring(string s) {
+    //     int maxLen = 0;
+    //     int left = 0;
+    //     unordered_set<char> st;
+    //     for(int right = 0; right < s.length(); right++){
+    //         while(st.count(s[right])) {
+    //             st.erase(s[left]);
+    //             left++;
+    //         }
 
-            st.insert(s[right]);
+    //         st.insert(s[right]);
+
+    //         maxLen = max(maxLen, right - left + 1);
+    //     }
+    //     return maxLen;
+    // }
+    int lengthOfLongestSubstring(string s) {
+        int last[128] = {};
+        int left = 0;
+        int maxLen = 0;
+
+        for(int right = 0; right < s.length(); right++) {
+            left = max(left, last[(unsigned char)s[right]]);
+
+            last[(unsigned char)s[right]] = right + 1;
 
             maxLen = max(maxLen, right - left + 1);
         }
+
         return maxLen;
     }
 };
